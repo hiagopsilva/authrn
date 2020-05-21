b7d429f91cf874e00723f75c93455b036953aae8
hiagopsilva <hiagohps360@gmail.com>
2020-05-21 19:50:37 -0300

    added auth services

---
+++ src/services/auth.ts
@@ -0,0 +1,13 @@
+export default function signIn() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        token: 'odcoscmasocmsaopcmsopmasipcoskcpaockosap',
+        user: {
+          name: 'Hiago',
+          email: 'hiago@gmail.com',
+        },
+      });
+    }, 2000);
+  });
+}
