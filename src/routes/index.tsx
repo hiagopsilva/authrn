6f3cb3dc0506bf3542aa99f8d445f32c4d991a3b
hiagopsilva <hiagohps360@gmail.com>
2020-05-21 19:50:07 -0300

    added routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
+
+const Routes: React.FC = () => {
+  return <AppRoutes />;
+}
+
+export default Routes;
