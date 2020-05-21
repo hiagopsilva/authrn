af0761dabc7f6f2fa4c60967e803a3e700c04f73
hiagopsilva <hiagohps360@gmail.com>
2020-05-21 19:34:07 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import 'react-native-gesture-handler';
 
-export default function App() {
+import React from 'react';
+import { View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
