db5fe00e17eef57afa41e83932e1e831f3325356
hiagopsilva <hiagohps360@gmail.com>
2020-05-21 19:48:32 -0300

    added SignIn page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Button} from 'react-native';
+
+const SignIn: React.FC = () => {
+  return (
+    <View>
+      <Button title="Sign In" onPress={() => {}} />
+    </View>
+  );
+}
+
+export default SignIn;
