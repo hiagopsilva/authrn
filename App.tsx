94d75cb9fc25305ef0f41d60469f452561ad72c7
hiagopsilva <hiagohps360@gmail.com>
2020-05-21 19:59:54 -0300

    added routes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
 
 const App: React.FC = () => {
   return (
-    
     <NavigationContainer>
-      <View />
+      <Routes />
     </NavigationContainer>
   );
 }
