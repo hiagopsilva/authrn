e424f1f4f4b222639c0c84b062717b56cd4eb8cc
hiagopsilva <hiagohps360@gmail.com>
2020-05-22 00:55:13 -0300

    added AuthProvider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/contexts/auth';
 
 import Routes from './src/routes';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+      <NavigationContainer>
+        <AuthProvider>
+          <Routes />    
+        </AuthProvider>
+      </NavigationContainer>
   );
 }
 
