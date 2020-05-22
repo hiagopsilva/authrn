6f12476663ad148b76104d5c359c341fbb9b2e07
hiagopsilva <hiagohps360@gmail.com>
2020-05-22 00:40:42 -0300

    feat: style and function

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Button} from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
+import { signIn } from '../../services/auth';
 
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: 'center',
+  }
+});
 const SignIn: React.FC = () => {
+  async function handleSignIn() {
+    const response = await signIn();
+
+    console.log(response);
+  }
+
   return (
-    <View>
-      <Button title="Sign In" onPress={() => {}} />
+    <View style={styles.container}>
+      <Button title="Sign in" onPress={handleSignIn} />
     </View>
   );
 }
